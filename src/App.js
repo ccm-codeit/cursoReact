faefe09e8fb5f3b1afec8694f68d0301dcabb29f
einar <einarlop0@gmail.com>
2021-03-19 15:44:12 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import OpinionCard from "./components/OpinionCard";
+import { useState } from "react";
 
 function App() {
+  const [card, setCard] = useState({
+    title: "",
+    description: "",
+  });
+
+  const [cards, setCards] = useState([]);
+
+  const handleOnChange = (e) => {
+    setCard({
+      ...card,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handelOnDelete = (index) => {
+    cards.filter(() => {
+      return setCards(cards.filter((card, i) => i !== index));
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setCards((cards) => [...cards, card]);
+    console.log(cards);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="Form-Wrapper">
+        <label className="Title-Contanier">
+          <span>Title</span>
+          <input
+            className="Title-Input"
+            name="title"
+            onChange={handleOnChange}
+          />
+        </label>
+
+        <label className="Desc-Contanier">
+          <span>Description</span>
+          <textarea
+            name="description"
+            onChange={handleOnChange}
+            className="Desc-Input"
+          />
+        </label>
+
+        <button className="Add-Button" onClick={onSubmit}>
+          Create
+        </button>
+      </form>
+
+      <div>
+        {cards.map((card, index) => {
+          return (
+            <OpinionCard
+              title={card.title}
+              description={card.description}
+              onDelete={() => handelOnDelete(index)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/OpinionCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/App.css";
+
+function OpinionCard(props) {
+  return (
+    <div className="Card-Wrapper">
+      <p className="Card-Title">{props.title}</p>
+      <p className="Card-Description">{props.description}</p>
+      <button className="Card-Delete" onClick={props.onDelete}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default OpinionCard;
