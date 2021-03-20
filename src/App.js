cdf36375077b8ec17cd3d98c75a5c068bdb33465
EricJ <ericjardon@hotmail.com>
2021-03-19 20:47:32 -0600

    Refactor handleOnDelete, add extra div for post content

--- src/App.js
+++ src/App.js
@@ -3,68 +3,80 @@ import OpinionCard from "./components/OpinionCard";
 import { useState } from "react";
 
 function App() {
-  const [card, setCard] = useState({
+  const [card, setCard] = useState({          // la información de los inputs
     title: "",
     description: "",
   });
 
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState([]);     // lista de nuestras 'Opinion Cards'
 
-  const handleOnChange = (e) => {
+  const handleOnChange = (e) => {             // manejador de cambios en input
     setCard({
       ...card,
       [e.target.name]: e.target.value,
     });
   };
 
-  const handelOnDelete = (index) => {
+  const handleOnDelete = (index) => {
     cards.filter(() => {
       return setCards(cards.filter((card, i) => i !== index));
     });
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = (e) => {       // agrega una nueva card a la lista de cards
+    e.preventDefault();           // evita que recargue la página
     setCards((cards) => [...cards, card]);
     console.log(cards);
   };
 
   return (
     <div className="App">
-      <form className="Form-Wrapper">
-        <label className="Title-Contanier">
-          <span>Title</span>
-          <input
-            className="Title-Input"
-            name="title"
-            onChange={handleOnChange}
-          />
-        </label>
-
-        <label className="Desc-Contanier">
-          <span>Description</span>
-          <textarea
-            name="description"
-            onChange={handleOnChange}
-            className="Desc-Input"
-          />
-        </label>
-
-        <button className="Add-Button" onClick={onSubmit}>
-          Create
-        </button>
-      </form>
-
-      <div>
-        {cards.map((card, index) => {
-          return (
-            <OpinionCard
-              title={card.title}
-              description={card.description}
-              onDelete={() => handelOnDelete(index)}
+      <div className="Main-Wrapper">
+        <div className="Main-Post">
+            <div>
+              <p className="Post-Title">Hello Title</p>
+            </div>
+            <div className="Content-Wrapper">
+              <p>Hello content</p>
+            </div>
+        </div>
+        <form className="Form-Wrapper">
+          <label className="Title-Contanier">
+            <span>Title</span>
+            <input
+              className="Title-Input"
+              name="title"
+              placeholder="My Opinion Title"
+              onChange={handleOnChange}
             />
-          );
-        })}
+          </label>
+
+          <label className="Desc-Contanier">
+            <span>Description</span>
+            <textarea
+              name="description"
+              placeholder="What do you think?" 
+              onChange={handleOnChange}
+              className="Desc-Input"
+            />
+          </label>
+
+          <button className="Add-Button" onClick={onSubmit}>
+            Add
+          </button>
+        </form>
+
+        <div>
+          {cards.map((card, index) => {
+            return (
+              <OpinionCard
+                title={card.title}
+                description={card.description}
+                onDelete={() => handleOnDelete(index)}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
